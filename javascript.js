7c560560e5ba399a1380da2329ae279d3caf0204
maxheerdegen <96018857+maxheerdegen@users.noreply.github.com>
2024-06-27 17:35:31 +0100

    add modal form to add new book

--- javascript.js
+++ javascript.js
@@ -21,6 +21,32 @@ function displayBooksOnPage(library){
 
 const library = [];
 const container = document.querySelector(".container");
+const showButton = document.querySelector(".showDialog");
+const newBookDialog = document.querySelector(".newBookDialog");
+const newBookForm = document.querySelector(".newBookForm");
+const confirmButton = document.querySelector(".confirm");
+
+
+const newBookTitle = document.querySelector("#title");
+const newBookAuthor = document.querySelector("#author");
+const newBookPages = document.querySelector("#pages");
+const newBookRead = document.querySelector("#read");
+
+showButton.addEventListener("click", () =>{
+    newBookDialog.showModal();
+})
+
+confirmButton.addEventListener("click", (event)=>{
+    event.preventDefault();
+    if (newBookRead.checked){
+        newBookRead.value = "yes";
+    }
+    addBookToLibrary(newBookTitle.value, newBookAuthor.value, newBookPages.value, newBookRead.value);
+    console.table(library);
+    newBookForm.reset();
+    newBookDialog.close();
+})
+
 
 addBookToLibrary("hobbit", "Tolkien", 500, true);
 addBookToLibrary("potter", "rowling", 500, true);
